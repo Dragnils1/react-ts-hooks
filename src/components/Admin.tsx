7dfc1be47d1b694ff55253fed3caad52538f0462
dragnils1 <m.sharabarin05@mail.ru>
2023-04-21 20:08:32 +0600

    created filter

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,13 +1,18 @@
-import React, { Suspense, useContext, useEffect, useState, useTransition } from "react";
+import React, { ChangeEvent, Suspense, useContext, useEffect, useState, useTransition } from "react";
 import { Context } from "./Context";
 
 interface ReqData { id: number, name: string, year: number, pantone_value: string, color: string }
+
+const filterBySearch = (entities: ReqData[], search: string): ReqData[] => 
+    entities.filter(item => item.name.includes(search))
+
 
 const Admin: React.FC = () => {
 
     const [data, setData] = useState<ReqData[]>([]);
     const [repeatReq, setRepeactReq] = useState(false)
     const [countData, setCountData] = useState(0)
+    const [search, setSearch] = useState('')
     const cont = useContext(Context)
     const [isPending, startTransition] = useTransition();
 
@@ -21,21 +26,27 @@ const Admin: React.FC = () => {
         if (returnable) return data.data 
     }
 
+    const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearch(e.target.value)
+    }
+
     useEffect(() => {
         // here react using batching for asink actions
         setTimeout(() => {
             getData(true)
             setCountData(countData + 1)
-        }, 2000)
+        }, 500)
         
     }, [repeatReq])
 
     return(
         <>
+            <h2>Enter kay words to filter array</h2>
+            <input type="text" onChange={handleSearch} value={search}/>
             <h1>{isPending ? 'Lazy loading...' : 'Data:'}</h1>
             <ul>
                 <Suspense fallback={<h1>Loading with suspence component ...</h1>} >
-                    {data.map((el) => {
+                    {filterBySearch(data, search).map((el) => {
                         return (
                             <li key={el.id} style={{color: el.color}}>
                                 Name: {el.name}, age: {el.year}
